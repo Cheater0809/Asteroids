2c1fb91b7f0cf214246a1781952791decc2702b8
Cheater0809 <tom.gemeinder@gmail.com>
2025-09-25 01:43:31 +0200

    implemented containers "updatable" and "drawable"

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable) # pyright: ignore[reportAttributeAccessIssue]
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
