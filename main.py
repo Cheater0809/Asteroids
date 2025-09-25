1178906252a59cf0a887587ca6fdf5af09e03c5a
Cheater0809 <tom.gemeinder@gmail.com>
2025-09-25 14:06:14 +0200

    added Asteroids

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import ast
 import pygame
+from asteroid import Asteroid
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -12,8 +15,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable) # pyright: ignore[reportAttributeAccessIssue]
+    Asteroid.containers = (asteroids, updatable, drawable) # pyright: ignore[reportAttributeAccessIssue]
+    AsteroidField.containers = (updatable,) # pyright: ignore[reportAttributeAccessIssue]
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
