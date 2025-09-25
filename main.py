3c470e0b1105ad1f161af21b7bc775577144645f
Cheater0809 <tom.gemeinder@gmail.com>
2025-09-25 13:36:05 +0200

    fixed player drawing

--- main.py
+++ main.py
@@ -10,17 +10,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable) # pyright: ignore[reportAttributeAccessIssue]
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         updatable.update(dt)
-        drawable.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
